f4b6245ecc426e4412a9ccfc077212863c837ef8
ab923837 <abdullahnegahban@ta393.wpa.dal.ca>
2017-04-09 16:02:39 -0300

    JUnit testing (Jafar, Abdullah N., Aisha)

--- src/main/java/testing/AdminAuthenticationTest.java
+++ src/main/java/testing/AdminAuthenticationTest.java
@@ -32,21 +32,22 @@ public class AdminAuthenticationTest {
    		
 		assertEquals(actualResult, expectedResult);
    }
-   /*
+   
    //Search for non-existing username test
    @Test
    public void testUsernameFail() {
-	   	sampleAdmins.add("admin1");
-   		sampleAdmins.add("admin2");
-   		sampleAdmins.add("admin3");
-   		boolean expectedResult = false;
+	   	CourseyUser user1 = new CourseyUser();
+	   	user1.setEmail("user1@coursey.com");
+	   	CourseyUser user2 = new CourseyUser();
+	   	user2.setEmail("user2@coursey.com");
+	   	CourseyUser user3 = new CourseyUser();
+		user3.setEmail("user3@coursey.com");
+	   	users.add(user1);
+	   	users.add(user2);
+	   	users.add(user3);
+   		boolean expectedResult = true;
    		boolean actualResult;
-   		if(al.findUsername((ArrayList)sampleAdmins, "non-existing") == -1){
-   			actualResult = false;
-   		}
-   		else{
-   			actualResult = true;
-   		}
+   		actualResult =(al.findUsername(users, "un-existing@coursey.com") == -1);
    		
 		assertEquals(actualResult, expectedResult);
    }
@@ -54,20 +55,25 @@ public class AdminAuthenticationTest {
    //Search for a correct password
    @Test
    public void testCorrectPassword() {
-	   	sampleAdmins.add("admin1");
-  		sampleAdmins.add("admin2");
-  		sampleAdmins.add("admin3");
-  		samplePasswords.add("pass1");
-  		samplePasswords.add("pass2");
-  		samplePasswords.add("pass3");
-  		
+	   	CourseyUser user1 = new CourseyUser();
+	   	user1.setPassword("pass1");
+	   	user1.setEmail("user1@coursey.com");
+	   	CourseyUser user2 = new CourseyUser();
+	   	user2.setPassword("pass2");
+	   	user2.setEmail("user2@coursey.com");
+	   	CourseyUser user3 = new CourseyUser();
+		user3.setPassword("pass3");
+		user3.setEmail("user3@coursey.com");
+	   	users.add(user1);
+	   	users.add(user2);
+	   	users.add(user3);
   		boolean expectedResult= true;
   		boolean actualResult;
   		
-  		int passwordIndex = al.findUsername(sampleAdmins, "admin2");
-  		String expectedPassword = samplePasswords.get(passwordIndex);
+  		int passwordIndex = al.findUsername(users, "user2@coursey.com");
+  		String expectedPassword = users.get(passwordIndex).getPassword();
   				
-  		if(al.findPassword(samplePasswords, expectedPassword, passwordIndex) == true){
+  		if(al.findPassword(users, expectedPassword, passwordIndex) == true){
   			actualResult = true;
   		}
   		else{
@@ -75,24 +81,30 @@ public class AdminAuthenticationTest {
   		}
 		assertEquals(actualResult, expectedResult);
   }
-   
+  
   //Search for a wrong password
   @Test
   public void testIncorrectPassword() {
-	   	sampleAdmins.add("admin1");
-  		sampleAdmins.add("admin2");
-  		sampleAdmins.add("admin3");
-  		samplePasswords.add("pass1");
-  		samplePasswords.add("pass2");
-  		samplePasswords.add("pass3");
+	   	CourseyUser user1 = new CourseyUser();
+	   	user1.setPassword("pass1");
+	   	user1.setEmail("user1@coursey.com");
+	   	CourseyUser user2 = new CourseyUser();
+	   	user2.setPassword("pass2");
+	   	user2.setEmail("user2@coursey.com");
+	   	CourseyUser user3 = new CourseyUser();
+		user3.setPassword("pass3");
+		user3.setEmail("user3@coursey.com");
+	   	users.add(user1);
+	   	users.add(user2);
+	   	users.add(user3);
   		
   		boolean expectedResult = false;
   		boolean actualResult;
   		
-  		int passwordIndex = al.findUsername((ArrayList)sampleAdmins, "admin2");
+  		int passwordIndex = al.findUsername(users, "user2@coursey.com");
   		String enteredPassword = "wrongPassword";
   				
-  		if(al.findPassword((ArrayList)samplePasswords, enteredPassword, passwordIndex) == true){
+  		if(al.findPassword(users, enteredPassword, passwordIndex) == true){
   			actualResult = true;
   		}
   		else{
@@ -101,5 +113,4 @@ public class AdminAuthenticationTest {
   		
 		assertEquals(actualResult, expectedResult);
   }
-  */
 } 
